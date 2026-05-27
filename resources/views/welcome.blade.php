a38897a44bb712a88e71372d61ccc3e2f22e6626
Nilay Pandya <nilay.pandya21@gmail.com>
2026-05-27 23:51:57 +0530

    Initial Laravel setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dynamic Quiz System</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-light">
+
+<div class="container py-5">
+
+    <div class="alert alert-success">
+        Laravel + Bootstrap + jQuery Setup Successful
+    </div>
+
+</div>
+
+</body>
+</html>
